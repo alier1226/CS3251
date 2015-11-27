1f623f1405db6d049f08a702f6eb20f6877c4114
Alier Hu <djyhu@hotmail.com>
2015-11-26 18:59:50 -0600

    client side finished

--- FxAServer.py
+++ FxAServer.py
@@ -5,45 +5,63 @@ from RxpServerSocket import RxpServerSocket
 import sys
 
 class fxaserver:
+
+    def __init__(self):
+        self.FXAPORT = 5000
+        self.EMUIP = 8081
+        self.EMUPORT = 'localhost'
+        self.STATE = 'welcome'
+        self.DEBUG = False
+        self.s = RxpServerSocket(self.DEBUG)
+        self.msg = ''
+        self.data =''
+        self.MAXTIMEOUT = 3
     def main(self):
-        FXAPORT = 5000
-        EMUIP = 8081
-        EMUPORT = 'localhost'
-        STATE = 'welcome'
-        DEBUG = False
-        s = RxpServerSocket(DEBUG)
         while(1):
-            if(STATE == 'welcome'):
+            if(self.STATE == 'welcome'):
                 if(len(sys.argv)<4):
                     print "invalid command"
                     break
                 try:
-                    FXAPORT = int(sys.argv[1])
+                    self.FXAPORT = int(sys.argv[1])
                 except:
                     print "invalid command"
 
-                if FXAPORT%2 != 1:
+                if self.FXAPORT%2 != 1:
                     print "Please enter a valid port number that the socket should bind to (must be even)"
                     print "set to default 8081"
-                EMUIP = sys.argv[2]
-                EMUPORT = int(sys.argv[3])
+                self.EMUIP = sys.argv[2]
+                self.EMUPORT = int(sys.argv[3])
                 if len(sys.argv) > 4:
                     if ((sys.argv[4] == 'd') or (sys.argv[4] == 'D')):
-                        DEBUG = True
+                        self.DEBUG = True
                     else:
                         print "Invalid debug command"
-                s = RxpServerSocket(DEBUG)
-                s.bind(EMUIP, EMUPORT, FXAPORT)
-                while not s.states["Connected"]:
-                    s.accept()
-                    if not s.states["Connected"]:
-                        print "FxA: Connection not established, trying again"
+                self.s = RxpServerSocket(self.DEBUG)
+                self.s.bind(self.EMUIP, self.EMUPORT, self.FXAPORT)
+                # self.s.listen()
 
-                while True:
-                    res = s.listen()
-                    if res == "post":
-                        s.recv()
-                    print "next"
-                    return
+                while not self.s.states["Connected"]:
+                    self.s.accept()
+                    if not self.s.states["Connected"]:
+                        command = raw_input("Welcome. Connection is not established. Please type command:")
+                        if command == 'terminate':
+                            self.STATE = 'terminate'
+                        else:
+                            self.msg = command.split(" ")
+                            if self.msg[0] == 'window':
+                                try:
+                                    self.s.setWindowSize(int (self.msg[1]))
+                                    self.STATE = 'window'
+                                except Exception,e:
+                                    print"can't change window size" + e
+            if(self.STATE == 'window'):
+                print('window size has been set to '+ str(self.s.windowSize))
+                self.STATE = 'welcome'
+            if(self.STATE == 'terminate'):
+                print "terminate server"
+                #TODO: close client socket
+                self.s.close()
+
 server = fxaserver()
 server.main()
